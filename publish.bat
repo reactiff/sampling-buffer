b8ebbba1d2d208475d529fdff668596487109140
Rick Ellis <riccello@yahoo.com>
2021-06-06 20:56:22 -0500

    Fix publish script for 4th time

--- publish.bat
+++ publish.bat
@@ -1,11 +1,15 @@
-echo "Publishing..."
-
-git add .
-
 @echo off
 set /p msg="Enter git commit message: "
+@echo on
 
+git add .
 git commit -m "%msg%"
+git push
 
+npm version patch
+npm publish
 
-echo Done
+yarn deploy
+
+echo
+echo Done!
